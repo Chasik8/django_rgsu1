b5cec405247f49da6fa24a7bc50a15a35d46bfed
никита часовких <hasnikita@mail.ru>
2023-06-17 22:47:01 +0300

    Albom

--- first/views.py
+++ first/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
+def index(reqest):
+    context = {
+        'name': "Никита",
+    }
+    a=reqest.GET.get('fisrt',None)
+    context['a']=a
+    return render(reqest, 'index.html', context)
+# Create your views here.
